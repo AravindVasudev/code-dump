3c775019df88b0283d32b4a977e5174448535a4b
AravindVasudev <aravindv96@yahoo.com>
2017-03-28 22:21:59 +0530

    26 days done.

---
+++ Hackerrank_30_days_of_code/day_24.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+class Solution:
+    def insert(self,head,data):
+            p = Node(data)
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def removeDuplicates(self,head):
+        #Write your code here
+        if not head: return None
+        s = {head.data}
+        previous = head
+        now = head.next
+        while now:
+            if now.data in s:
+                previous.next = now.next
+                now = now.next
+            else:
+                s.add(now.data)
+                previous = now
+                now = now.next
+
+
+
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)
+head=mylist.removeDuplicates(head)
+mylist.display(head); 
---
+++ Hackerrank_30_days_of_code/day_26.py
@@ -0,0 +1,11 @@
+rd, rm, ry = map(int, input().split(' '))
+ed, em, ey = map(int, input().split(' '))
+
+if (ry, rm, rd) <= (ey, em, ed):
+    print(0)
+elif (ry, rm) == (ey, em):
+    print(15 * (rd - ed))
+elif ry == ey:
+    print(500 * (rm - em))
+else:
+    print(10000)
