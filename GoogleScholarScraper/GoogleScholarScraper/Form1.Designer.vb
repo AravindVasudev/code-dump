cb1250eb360ae42a4e26698b614f9d8c5151e5d3
Aravind Vasudevan <aravindv96@yahoo.com>
2017-09-04 04:22:35 +0530

    Added UI

---
+++ GoogleScholarScraper/GoogleScholarScraper/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.txtScholarID = New System.Windows.Forms.TextBox()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.browser = New System.Windows.Forms.WebBrowser()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Courier New", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(87, Byte), Integer), CType(CType(34, Byte), Integer))
+        Me.lblTitle.Location = New System.Drawing.Point(40, 64)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(197, 22)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "Enter Scholar ID:"
+        '
+        'txtScholarID
+        '
+        Me.txtScholarID.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(87, Byte), Integer), CType(CType(34, Byte), Integer))
+        Me.txtScholarID.Font = New System.Drawing.Font("Courier New", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtScholarID.ForeColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(33, Byte), Integer), CType(CType(33, Byte), Integer))
+        Me.txtScholarID.Location = New System.Drawing.Point(41, 126)
+        Me.txtScholarID.Name = "txtScholarID"
+        Me.txtScholarID.Size = New System.Drawing.Size(193, 29)
+        Me.txtScholarID.TabIndex = 1
+        Me.txtScholarID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnGo
+        '
+        Me.btnGo.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(87, Byte), Integer), CType(CType(34, Byte), Integer))
+        Me.btnGo.Cursor = System.Windows.Forms.Cursors.Cross
+        Me.btnGo.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnGo.Font = New System.Drawing.Font("Courier New", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnGo.ForeColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(33, Byte), Integer), CType(CType(33, Byte), Integer))
+        Me.btnGo.Location = New System.Drawing.Point(95, 195)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(75, 31)
+        Me.btnGo.TabIndex = 2
+        Me.btnGo.Text = "GO"
+        Me.btnGo.UseVisualStyleBackColor = False
+        '
+        'browser
+        '
+        Me.browser.Location = New System.Drawing.Point(12, 12)
+        Me.browser.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.browser.Name = "browser"
+        Me.browser.Size = New System.Drawing.Size(122, 108)
+        Me.browser.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.btnGo
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(33, Byte), Integer), CType(CType(33, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnGo)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.txtScholarID)
+        Me.Controls.Add(Me.browser)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Scholar Extractor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents txtScholarID As TextBox
+    Friend WithEvents btnGo As Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents browser As WebBrowser
+End Class
---
+++ GoogleScholarScraper/GoogleScholarScraper/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Dim ScholarURL As String = "https://scholar.google.co.in/citations?user="
+
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        ScholarURL &= txtScholarID.Text ' Construct URL
+
+        Try
+            browser.Navigate(ScholarURL)
+            Dim name = browser.Document.GetElementById("gsc_prf_in").InnerText
+
+            MessageBox.Show(name)
+        Catch ex As Exception
+            MessageBox.Show("An Exception Occured!")
+        End Try
+    End Sub
+End Class
