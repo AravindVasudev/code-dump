088ebe182a211478ff8261b802015100078f55c8
AravindVasudev <aravindv96@yahoo.com>
2018-08-28 17:59:18 +0530

    Added TOEFL speaking recorder

---
+++ toefl-speaking-recorder.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Global colors
+red="\033[0;31m"
+orange="\033[0;33m"
+blue="\033[0;34m"
+
+# takes seconds as input and counts down
+function countdown() {
+    for (( i = $1; i > 0; i-- )); do
+        printf "${red}$i \r"
+        sleep 1
+    done
+}
+
+# Type picker
+echo "1. 15 sec practice & 45 sec speaking \n2. 30 sec practice & 60 sec speaking"
+read -n1 -p "Choose: " choice
+if [ "$choice" = "1" ]; then
+    practice=5
+    speaking=5
+else
+    practice=30
+    speaking=60
+fi
+
+echo ""
+read -p "filename to output recording: " filename
+
+# practice countdown
+printf "${orange}PRACTICE:\n"
+countdown $practice
+
+# speaking countdown with recording
+printf "${blue}\nSPEAK:\n"
+arecord -d "${speaking}" -f cd "${filename}" &
+countdown $speaking
+wait
